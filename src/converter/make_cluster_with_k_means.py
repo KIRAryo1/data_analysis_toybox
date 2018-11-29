629b083ef75c08c3056f32255a79641a5e670cb3
KIRAryo1 <r.kira@grooves.co.jp>
2018-11-29 20:48:29 +0900

    Temp! cluster users by bagdes

---
+++ src/converter/make_cluster_with_k_means.py
@@ -0,0 +1,13 @@
+import pathlib as pth
+import numpy as np
+import pandas as pd
+import scipy as sp
+import sklearn as skl
+
+file_in = 'data/raw/user_badge_in.csv'
+
+badge_list = pd.read_csv(file_in)
+
+file_out = 'data/processed/user_badge_cluster_with_k_means.csv'
+
+

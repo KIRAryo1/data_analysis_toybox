3926b9a87a8938e743c281a217674be55aa4c871
KIRAryo1 <r.kira@grooves.co.jp>
2018-12-03 20:48:45 +0900

    calculate badge similarity

--- src/converter/make_cluster_with_k_means.py
+++ src/converter/make_cluster_with_k_means.py
@@ -12,8 +12,20 @@ file_out = 'data/processed/user_badge_cluster_with_k_means.csv'
 
 def process_make_user_coordinate(raw):
     ret = raw.groupby(['id', 'name']).max()
-    print(raw.sort_values(['id', 'name', 'rank']).head(100).to_string())
     return ret
+
+def badge_similarity(b1, b2):
+    if b1.index[0][1] != b2.index[0][1]:
+        return 0
+    else:
+        return b1.iat[0,0] * b2.iat[0,0] / (1 + abs(b1.iat[0,0] - b2.iat[0,0]))
+
+u_b = process_make_user_coordinate(badge_list)
+print(u_b[1:20])
+print(badge_similarity(u_b[1:2], u_b.iloc[6:7]))
+
+
+
 
 with open(file_out, mode='w') as f:
     f.write(process_make_user_coordinate(badge_list).to_string())
--- src/helper/define_distance.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-
-def language_singleton_badge_distance(b1, b2):

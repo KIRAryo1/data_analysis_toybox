21d4bfd1af0a7afceb8b24095320c79e4172f1d6
KIRAryo1 <r.kira@grooves.co.jp>
2018-12-03 19:58:51 +0900

    Complement defected data

--- src/converter/make_cluster_with_k_means.py
+++ src/converter/make_cluster_with_k_means.py
@@ -10,4 +10,9 @@ badge_list = pd.read_csv(file_in)
 
 file_out = 'data/processed/user_badge_cluster_with_k_means.csv'
 
+def process_make_user_coordinate(raw):
+    ret = raw.sort_values(user_id)
+    return ret
 
+with open(file_out, mode='w') as f:
+    f.write(process_make_user_coordinate(badge_list))
---
+++ src/helper/define_distance.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+def language_singleton_badge_distance(b1, b2):

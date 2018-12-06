db46ef7e4a0f76d43ff65b53c1abdf44e16cc866
KIRAryo1 <r.kira@grooves.co.jp>
2018-12-06 20:48:46 +0900

    Implement user similarity

--- src/converter/make_cluster_with_k_means.py
+++ src/converter/make_cluster_with_k_means.py
@@ -11,21 +11,32 @@ badge_list = pd.read_csv(file_in)
 file_out = 'data/processed/user_badge_cluster_with_k_means.csv'
 
 def process_make_user_coordinate(raw):
-    ret = raw.groupby(['id', 'name']).max()
+    ret = raw.groupby(['id', 'name']).max().reset_index()
     return ret
 
 def badge_similarity(b1, b2):
-    if b1.index[0][1] != b2.index[0][1]:
+    if b1.iat[1] != b2.iat[1]:
         return 0
     else:
-        return b1.iat[0,0] * b2.iat[0,0] / (1 + abs(b1.iat[0,0] - b2.iat[0,0]))
+        return b1.iat[2] * b2.iat[2] / (1 + abs(b1.iat[2] - b2.iat[2]))
 
 u_b = process_make_user_coordinate(badge_list)
-print(u_b[1:20])
-print(badge_similarity(u_b[1:2], u_b.iloc[6:7]))
+print(badge_similarity(u_b.iloc[1,:], u_b.iloc[5,:]))
+print(badge_similarity(u_b.iloc[1,:], u_b.iloc[6,:]))
 
+u1 = u_b[1:5]
+u2 = u_b[5:9]
 
+def user_similarity(u1, u2):
+    sim = 0
+    for index1, row1 in u1.iterrows():
+        print(type(row1))
+        for index2, row2 in u2.iterrows():
+            sim += badge_similarity(row1, row2)
 
+    return sim
+
+print(user_similarity(u1, u2))
 
 with open(file_out, mode='w') as f:
     f.write(process_make_user_coordinate(badge_list).to_string())

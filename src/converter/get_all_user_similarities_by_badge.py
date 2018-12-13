b0896457afa408468dcd5d2b0bb9c8f3bd1ff586
KIRAryo1 <r.kira@grooves.co.jp>
2018-12-13 20:13:11 +0900

    User similarity by badge for all users

--- src/converter/get_all_user_similarities_by_badge.py
+++ src/converter/get_all_user_similarities_by_badge.py
@@ -8,8 +8,6 @@ file_in = 'data/raw/user_badge_in.csv'
 
 badge_list = pd.read_csv(file_in)
 
-file_out = 'data/processed/user_badge_cluster_with_k_means.csv'
-
 def process_make_user_coordinate(raw):
     ret = raw.groupby(['id', 'name']).max().reset_index()
     return ret
@@ -22,9 +20,6 @@ def badge_similarity(b1, b2):
 
 u_b = process_make_user_coordinate(badge_list)
 
-u1 = u_b[1:5]
-u2 = u_b[5:9]
-
 def user_similarity(u1, u2):
     sim = 0
     for index1, row1 in u1.iterrows():
@@ -33,6 +28,20 @@ def user_similarity(u1, u2):
 
     return sim
 
+def get_all_similarities(u_b, f):
+    u_b_group_dict = u_b.groupby(['id'])
+    user_ids = u_b_group_dict.groups.keys()
+    for uid1 in user_ids:
+        for uid2 in user_ids:
+            if uid1 < uid2:
+                u1 = u_b_group_dict.get_group(uid1)
+                u2 = u_b_group_dict.get_group(uid2)
+                f.write(str(uid1)+', '+str(uid2)+'  '+str(user_similarity(u1, u2))+'\n')
+
+file_out = 'data/interim/badges_sorted_by_users.csv'
 with open(file_out, mode='w') as f:
-    f.write(str(user_similarity(u1, u2)))
     f.write(process_make_user_coordinate(badge_list).to_string())
+
+file_out = 'data/processed/user_similarities_by_badge.csv'
+with open(file_out, mode='w') as f:
+    get_all_similarities(u_b, f)
